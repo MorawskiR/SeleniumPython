ce1fb655b1daa815ba77843e9a23e91d853ccbe5
Rafal Morawski <rafal_morawski@epam.com>
2023-11-10 22:26:53 +0100

    more chars

--- Test4.py
+++ Test4.py
@@ -8,12 +8,17 @@ driver = webdriver.Chrome()
 
 driver.get("https://helion.pl")
 
-szukaj = driver.find_element(By.ID,'inputSearch').send_keys('Python' + Keys.ENTER)
+szukaj = driver.find_element(By.ID,'inputSearch')
 
 #webdriver.ActionChains(driver).key_down(Keys.CONTROL).send_keys("a").perform()
 #wciska kontrol i nie puszczając go wcisnie litere a = zaznaczenie wszystkiego na stronie
 
 action = webdriver.ActionChains(driver)
 
-action.key_down(Keys.SHIFT).send_keys("a").perform()
+action.key_down(Keys.SHIFT).send_keys_to_element(szukaj, 'python').key_up(Keys.SHIFT).send_keys('python').perform()
+#wyszukuje input search i wpisuje tam python a nastepnie bez shifta
 
+
+szukaj.clear()
+#wyslanie do pola komende wyczyszczenia go
+time.sleep(50)
