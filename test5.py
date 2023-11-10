1874e313acfd7e26cd3bba9d59464d4a869593c8
Rafal Morawski <rafal_morawski@epam.com>
2023-11-10 22:49:36 +0100

    dziala

--- test5.py
+++ test5.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -5,13 +7,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 
-driver.get("///C:\Users\Rafal_Morawski\Desktop\Videopoint")
-
+driver.get("///C:/Users/Rafal_Morawski/desktop/Videopoint/alerty.html")
+# ///C:\Users\Rafal_Morawski\Desktop\Videopoint
+# time.sleep(10)
 driver.find_element(By.LINK_TEXT, "Kliknij, aby wyświetlić okienko alert").click()
+
 #próba wlaczenia okienka
 
-
-alert = WebDriverWait(driver,timeout = 5).until(expected_conditions.alert_is_presented())
+time.sleep(5)
+alert = WebDriverWait(driver, timeout=5).until(expected_conditions.alert_is_present())
 text = alert.text
 
 alert.accept()
