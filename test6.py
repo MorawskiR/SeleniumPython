5413cfbb8e9aafff8968c686d6b31fd0e5867f8f
Rafal Morawski <rafal_morawski@epam.com>
2023-11-11 15:03:46 +0100

    cookies done

--- test6.py
+++ test6.py
@@ -5,7 +5,14 @@ driver = webdriver.Chrome()
 #driver.get("https://helion.pl")
 driver.get(url='https://helion.pl')
 
+#driver.add_cookie({"name":"tomek","value":"selenium"})
+#zmiana dodanie cookies
+#driver.delete_all_cookies() #usuniecie wszystkich cookies
 
-#aktualny stan cookies
-print(driver.get_cookies())
+driver.delete_cookie("tomek") #podajemy wartosc nie pole
+
+# #aktualny stan cookies
+# print(driver.get_cookies())
 #pobranie cookies
+
+print(driver.get_cookie("name"))
