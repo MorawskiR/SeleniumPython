888c05bd52b8393e62cd9d4d83b8cd6bc2f69dba
Rafal Morawski <rafal_morawski@epam.com>
2023-11-15 15:30:59 +0100

    completed

--- test10.py
+++ test10.py
@@ -5,3 +5,10 @@ driver = webdriver.Chrome()
 
 driver.get("https://helion.pl")
 
+#driver.maximize_window()
+
+el = driver.find_element(By.CLASS_NAME,'promotion-book')
+
+el.screenshot('promocja1.png')
+#driver.save_screenshot('zrzut3.png') #robi zrzut ekranu o nazwie ('zrzut')
+
